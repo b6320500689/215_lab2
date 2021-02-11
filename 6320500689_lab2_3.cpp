d882a065653fe0506a2a4632be6ffa528a633282
b6320500689 <supitchaya.r@ku.th>
2021-02-11 16:03:09 +0700

    space

--- 6320500689_lab2_3.cpp
+++ 6320500689_lab2_3.cpp
@@ -5,24 +5,28 @@ int main (){
 	char x[5];
 	int y[5];
 	scanf("%d %d %d" ,&a,&b,&c);
-	if(a>b){
+	if(a>b)
+	{
 		j=a;
 		a=b;
 		b=j;
 	}
-	if(a>c){
+	if(a>c)
+	{
 		j=a;
 		a=c;
 		c=j;
 	}
-	if(b>c){
+	if(b>c)
+	{
 		j=b;
 		b=c;
 		c=j;
 	}
 	scanf("%s",&x);
 	
-	for(i=0;i<3;i++){
+	for(i=0;i<3;i++)
+	{
 		if(x[i] == 'A')
 			y[i] = a;
 		else if(x[i] == 'B')
@@ -30,7 +34,10 @@ int main (){
 		else if(x[i] == 'C')
 			y[i] = c;
 	}
+	
 	for(i=0;i<3;i++)
-	printf("%d ",y[i]);
+	{
+		printf("%d ",y[i]);
+	}
 	return 0;
 }
